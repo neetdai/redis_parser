c81d83655ba636aff81857fd17f8b3a33703c308
neetdai <>
2024-08-25 22:18:42 +0800

    feat: implement lexer and parser for redis protocol

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,12 @@
 use std::iter::{Iterator, Peekable};
 use std::str::CharIndices;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Token<'a> {
     SimpleString(&'a str),
     Error(&'a str),
+    Number(&'a str),
+    ChunkString(Vec<&'a str>),
 }
 
 #[derive(Debug)]
@@ -24,15 +26,22 @@ impl<'a> Lexer<'a> {
     }
 
     fn skip_line(&mut self) {
-        if self.scanner.next_if(|(_, c)| *c == '\r').is_some() {
-            self.position += 1;
-            if self.scanner.next_if(|(_, c)| *c == '\n').is_some() {
-                self.position += 1;
-            }
+        if self.inner.get(self.position..=self.position + 1).is_some() {
+            self.next_if(|(_, c)| *c == '\r');
+            self.next_if(|(_, c)| *c == '\n');
         }
     }
 
-    fn scan_string<F>(&mut self, condition: F) -> &'a str
+    fn next_if<F>(&mut self, condition: F) -> Option<(usize, char)> where F: FnOnce(&(usize, char)) -> bool {
+        self.scanner
+            .next_if(condition)
+            .map(|c| {
+                self.position += 1;
+                c
+            })
+    }
+
+    fn scan_string<F>(&mut self, condition: F) -> Option<&'a str>
     where
         F: FnOnce(&(usize, char)) -> bool + Copy,
     {
@@ -41,31 +50,45 @@ impl<'a> Lexer<'a> {
         while let Some((position, _)) = self.scanner.next_if(condition) {
             end_position = position;
         }
-        let (left_str, _) = self.inner.split_at(end_position);
-        let (_, right_str) = left_str.split_at(start_position);
-        right_str
+        let text = self.inner.get(start_position..=end_position)?;
+        self.position = end_position + 1;
+        Some(text)
+    }
+
+    fn scan_number(&mut self) -> (usize, usize) {
+        let start_position = self.position;
+        let mut end_position = self.position;
+        while let Some((position, _)) = self.scanner.next_if(|(_, c)| c.is_ascii_digit()) {
+            end_position = position;
+        }
+        (start_position, end_position)
     }
 
     fn scan_simple_string(&mut self) -> Option<Token<'a>> {
-        let (postion, _) = self.scanner.next_if(|(_, c)| *c == '+')?;
-        self.position = postion;
-        let token = {
-            let text = self.scan_string(|(_, c)| *c != '\r' && *c != '\n');
-            Token::SimpleString(text)
-        };
+        self.next_if(|(_, c)| *c == '+')?;
+        let text = self.scan_string(|(_, c)| *c != '\r' && *c != '\n')?;
         self.skip_line();
-        Some(token)
+        Some(Token::SimpleString(text))
     }
 
     fn scan_error(&mut self) -> Option<Token<'a>> {
-        let (postion, _) = self.scanner.next_if(|(_, c)| *c == '-')?;
-        self.position = postion;
-        let token = {
-            let text = self.scan_string(|(_, c)| *c != '\r' && *c != '\n');
-            Token::Error(text)
-        };
+        self.next_if(|(_, c)| *c == '-')?;
+        let text = self.scan_string(|(_, c)| *c != '\r' && *c != '\n')?;
         self.skip_line();
-        Some(token)
+        Some(Token::Error(text))
+    }
+
+    fn scan_integer(&mut self) -> Option<Token<'a>> {
+        self.next_if(|(_, c)| *c == ':')?;
+        let symbol_position = {
+            self.next_if(|(_, c)| *c == '+' || *c == '-')
+                .unwrap_or((self.position, '+'))
+                .0
+        };
+        let (_, end_position) = self.scan_number();
+        let text = self.inner.get(symbol_position..=end_position)?;
+        self.skip_line();
+        Some(Token::Number(text))
     }
 }
 
@@ -76,6 +99,7 @@ impl<'a> Iterator for Lexer<'a> {
         match self.scanner.peek() {
             Some((_, '+')) => self.scan_simple_string(),
             Some((_, '-')) => self.scan_error(),
+            Some((_, ':')) => self.scan_integer(),
             Some(_) => {
                 todo!()
             }
@@ -97,5 +121,63 @@ impl<'a> Parser<'a> {
 
     pub fn parse(&self) {
         let mut lexer = Lexer::new(self.buf);
+    }
+}
+
+mod tests {
+    use super::{
+        Lexer,
+        Token,
+    };
+
+    #[test]
+    fn test_simple_string() {
+        let mut lexer = Lexer::new("+OK\r\n");
+        assert_eq!(lexer.next().unwrap(), Token::SimpleString("OK"));
+        assert_eq!(lexer.next(), None);
+    }
+
+    #[test]
+    fn test_simple_string_2() {
+        let mut lexer = Lexer::new("+OK\r\n+OK\r\n");
+        assert_eq!(lexer.next().unwrap(), Token::SimpleString("OK"));
+        assert_eq!(lexer.next().unwrap(), Token::SimpleString("OK"));
+        assert_eq!(lexer.next(), None);
+    }
+
+    #[test]
+    fn test_error() {
+        let mut lexer = Lexer::new("-ERR unknown command 'FOO'\r\n");
+        assert_eq!(lexer.next().unwrap(), Token::Error("ERR unknown command 'FOO'"));
+        assert_eq!(lexer.next(), None);
+    }
+
+    #[test]
+    fn test_error_2() {
+        let mut lexer = Lexer::new("-ERR unknown command 'FOO'\r\n-10086\r\n");
+        assert_eq!(lexer.next().unwrap(), Token::Error("ERR unknown command 'FOO'"));
+        assert_eq!(lexer.next().unwrap(), Token::Error("10086"));
+        assert_eq!(lexer.next(), None);
+    }
+
+    #[test]
+    fn test_number() {
+        let mut lexer = Lexer::new(":1000\r\n");
+        assert_eq!(lexer.next().unwrap(), Token::Number("1000"));
+        assert_eq!(lexer.next(), None);
+
+        let mut lexer = Lexer::new(":+0\r\n");
+        assert_eq!(lexer.next().unwrap(), Token::Number("+0"));
+
+        let mut lexer = Lexer::new(":-0\r\n");
+        assert_eq!(lexer.next().unwrap(), Token::Number("-0"));
+    }
+
+    #[test]
+    fn test_number_2() {
+        let mut lexer = Lexer::new(":1000\r\n:-1000\r\n");
+        assert_eq!(lexer.next().unwrap(), Token::Number("1000"));
+        assert_eq!(lexer.next().unwrap(), Token::Number("-1000"));
+        assert_eq!(lexer.next(), None);
     }
 }
